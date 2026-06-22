898b9c035d709d64da982207b15ad88798681a0b
AVINASH RAI <76801210+avinash799@users.noreply.github.com>
2026-06-22 10:42:00 +0530

    solution

---
+++ majority_elementsinarray.cpp
@@ -0,0 +1,25 @@
+
+//i have use moore's voting algorithm to solve this problem 
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt = 0;
+        int el = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(cnt == 0) {
+                el = nums[i];
+                cnt = 1;
+            }
+            else if(nums[i] == el) {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+
+        return el;
+    }
+};
