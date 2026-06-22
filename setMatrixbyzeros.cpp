8901c9cfb86bb8675f5ca747df57c25488df77db
AVINASH RAI <76801210+avinash799@users.noreply.github.com>
2026-06-22 12:34:33 +0530

    set matrix by zeroes solution

---
+++ setMatrixbyzeros.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    void markRow(vector<vector<int>>& matrix, int row) {
+        int m = matrix[0].size();
+
+        for(int j = 0; j < m; j++) {
+            if(matrix[row][j] != 0) {
+                matrix[row][j] = -1;
+            }
+        }
+    }
+
+    void markCol(vector<vector<int>>& matrix, int col) {
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i++) {
+            if(matrix[i][col] != 0) {
+                matrix[i][col] = -1;
+            }
+        }
+    }
+
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == 0) {
+                    markRow(matrix, i);
+                    markCol(matrix, j);
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == -1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
