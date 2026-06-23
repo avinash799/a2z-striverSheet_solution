30a7453134a4453aa905d0329fdb2c06aa12ead0
AVINASH RAI <76801210+avinash799@users.noreply.github.com>
2026-06-23 13:38:17 +0530

    largest subarray with sum 0

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,26 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> fourSum(vector<int> &nums, int target)
+    {
 
         int n = nums.size();
         vector<vector<int>> ans;
 
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
 
-            if(i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            for(int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < n; j++)
+            {
 
-                if(j > i + 1 && nums[j] == nums[j - 1])
+                if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;
 
                 int k = j + 1;
                 int m = n - 1;
 
-                while(k < m) {
+                while (k < m)
+                {
 
                     long long sum =
                         (long long)nums[i] +
@@ -28,27 +33,29 @@ public:
                         nums[k] +
                         nums[m];
 
-                    if(sum < target) {
+                    if (sum < target)
+                    {
                         k++;
                     }
-                    else if(sum > target) {
+                    else if (sum > target)
+                    {
                         m--;
                     }
-                    else {
+                    else
+                    {
 
                         ans.push_back(
-                            {nums[i], nums[j], nums[k], nums[m]}
-                        );
+                            {nums[i], nums[j], nums[k], nums[m]});
 
                         k++;
                         m--;
 
-                        while(k < m &&
-                              nums[k] == nums[k - 1])
+                        while (k < m &&
+                               nums[k] == nums[k - 1])
                             k++;
 
-                        while(k < m &&
-                              nums[m] == nums[m + 1])
+                        while (k < m &&
+                               nums[m] == nums[m + 1])
                             m--;
                     }
                 }
